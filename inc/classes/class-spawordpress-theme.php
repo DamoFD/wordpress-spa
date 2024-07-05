b2456b9c4e259fcaed3fcb853d1ed47dec2025f0
DamoFD <voshall@yahoo.com>
2024-07-05 00:14:26 -0700

    dynamic nav menu

--- inc/classes/class-spawordpress-theme.php
+++ inc/classes/class-spawordpress-theme.php
@@ -15,9 +15,10 @@ class SPAWORDPRESS_THEME
 
     protected function __construct()
     {
-        // load class.
+        // Load class.
 
         Assets::get_instance();
+        Menus::get_instance();
 
         $this->setup_hooks();
     }
@@ -33,6 +34,7 @@ class SPAWORDPRESS_THEME
     public function setup_theme()
     {
         add_theme_support('title-tag');
+
         add_theme_support('custom-logo', [
             'header-text' => ['site-title', 'site-description'],
             'height' => 100,
@@ -40,5 +42,41 @@ class SPAWORDPRESS_THEME
             'flex-height' => true,
             'flex-width' => true,
         ]);
+
+        add_theme_support('custom-background', [
+            'default-color' => '#fff',
+            'default-image' => '',
+            'default-repeat' => 'no-repeat',
+        ]);
+
+        add_theme_support('post-thumbnails');
+
+        add_theme_support('customize-selective-refresh-widgets');
+
+        add_theme_support('automatic-feed-links');
+
+        add_theme_support(
+            'html5',
+            [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+            ]
+        );
+
+        add_editor_style();
+
+        add_theme_support('wp-block-styles');
+
+        add_theme_support('align-wide');
+
+        global $content_width;
+        if (!isset($content_width)) {
+            $content_width = 1240;
+        }
     }
 }
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -4,14 +4,21 @@
 *
 * @package spawordpress
 */
+
+$menu_class = \SPAWORDPRESS_THEME\Inc\Menus::get_instance();
+$header_menu_id = $menu_class->get_menu_id('spawordpress-header-menu');
+
+$header_menus = wp_get_nav_menu_items($header_menu_id);
 ?>
 
 <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <?php if (function_exists('the_custom_logo')) {
-            the_custom_logo();
-        }
-        ?>
+  <div class="flex items-center flex-shrink-0 text-white mr-6 space-x-2">
+        <div class="w-10">
+            <?php if (function_exists('the_custom_logo')) {
+                the_custom_logo();
+            }
+            ?>
+        </div>
     <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
   </div>
   <div class="block lg:hidden">
@@ -20,17 +27,27 @@
     </button>
   </div>
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <?php
+        if (!empty($header_menus) && is_array($header_menus)):
+    ?>
     <div class="text-sm lg:flex-grow">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Docs
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Examples
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-        Blog
-      </a>
+        <?php foreach($header_menus as $menu_item): ?>
+            <?php if (!$menu_item->menu_item_parent): ?>
+                <?php
+                    $child_menu_items = $menu_class->get_child_menu_items($header_menus, $menu_item->id);
+                    $has_children = !empty($child_menu_items) && is_array($child_menu_items);
+                ?>
+                <?php if (!$has_children): ?>
+                    <a href="<?=esc_url( $menu_item->url )?>" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        <?=esc_html($menu_item->title)?>
+                    </a>
+                <?php else: ?>
+                    <!-- TODO: dropdown structure -->
+                <?php endif; ?>
+            <?php endif; ?>
+        <?php endforeach; ?>
     </div>
+    <?php endif; ?>
     <div>
       <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
     </div>
