598f3ee3bd37d59533aa9ab3b42dbffe6c461e26
DamoFD <voshall@yahoo.com>
2024-07-05 21:47:17 -0700

    excerpt length

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -1,1 +1,31 @@
 <?php
+/**
+* Template for entry content.
+*
+* To be used inside WordPress The Loop.
+*
+* @package spawordpress
+*/
+?>
+
+<div class="entry-content">
+    <?php
+        if (is_single()) {
+            the_content(
+                sprintf(
+                    wp_kses(
+                        __('Continue reading %s <span>&rarr;</span>', 'spawordpress'),
+                            [
+                                'span' => [
+                                    'class' => []
+                                ]
+                            ],
+                    ),
+                    the_title('<span>"', '"</span>', false)
+                )
+            );
+        } else {
+            spawordpress_the_excerpt(50);
+        }
+    ?>
+</div>
