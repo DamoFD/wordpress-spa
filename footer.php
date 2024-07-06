311da3860c6e905a710dbbe89cc8a7fdae347cc7
DamoFD <voshall@yahoo.com>
2024-07-06 00:22:48 -0700

    display footer sidebar

--- footer.php
+++ footer.php
@@ -6,7 +6,14 @@
 */
 ?>
 
-        <footer>Footer</footer>
-        <?php wp_footer(); ?>
-    </body>
+<footer>
+    <h3>Footer</h3>
+    <?php if (is_active_sidebar('sidebar-2')): ?>
+        <aside>
+            <?php dynamic_sidebar('sidebar-2'); ?>
+        </aside>
+    <?php endif; ?>
+</footer>
+<?php wp_footer(); ?>
+</body>
 </html>
