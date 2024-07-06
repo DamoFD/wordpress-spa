08334901b68d979d57765e5fd430e3151e3d39a7
DamoFD <voshall@yahoo.com>
2024-07-05 20:21:48 -0700

    get post title

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -7,6 +7,7 @@
 
 $the_post_id = get_the_ID();
 $hide_title = get_post_meta($the_post_id, '_hide_page_title', true);
+$heading_class = !empty($hide_title) && 'yes' === $hide_title ? 'hidden' : '';
 $has_post_thumbnail = get_the_post_thumbnail();
 ?>
 
@@ -23,4 +24,20 @@ $has_post_thumbnail = get_the_post_thumbnail();
             ); ?>
             </a>
     <?php endif; ?>
+
+    <!-- Title -->
+    <?php if (is_single() || is_page()) {
+        printf(
+            '<h1 class="text-3xl font-extrabold %1$s">%2$s</h1>',
+            esc_attr($heading_class),
+            wp_kses_post(get_the_title())
+        );
+    } else {
+        printf(
+            '<h2 class="text-xl mb-8 font-extrabold"><a class="hover:underline" href="%1$s">%2$s</a></h2>',
+            esc_url(get_the_permalink()),
+            wp_kses_post(get_the_title())
+        );
+    }
+    ?>
 </header>
