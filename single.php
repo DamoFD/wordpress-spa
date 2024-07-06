dfdd90758ac703ba04a296d2cdc7254ec226b492
DamoFD <voshall@yahoo.com>
2024-07-05 22:05:47 -0700

    single post

--- single.php
+++ single.php
@@ -8,6 +8,19 @@
 get_header();
 ?>
 
-<div>Single Post</div>
+<div id="primary">
+    <main id="main" class="max-w-7xl mx-auto mt-5" role="main">
+        <?php if (is_home() && !is_front_page()): ?>
+            <h1 class="text-3xl font-extrabold mb-8"><?php single_post_title(); ?></h1>
+        <?php endif; ?>
+        <?php if (have_posts()): ?>
+            <?php while (have_posts()): the_post(); ?>
+                <?php get_template_part('template-parts/content'); ?>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <?php get_template_part('template-parts/content-none'); ?>
+        <?php endif; ?>
+    </main>
+</div>
 
 <?php get_footer(); ?>
