982146d05d0b3c58bb7065630efa2b15879a12b9
DamoFD <voshall@yahoo.com>
2024-07-06 00:02:15 -0700

    register sidebars

---
+++ inc/classes/class-sidebars.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* Register Sidebars
+*
+* @package spawordpress
+*/
+
+namespace SPAWORDPRESS_THEME\Inc;
+
+use SPAWORDPRESS_THEME\Inc\Traits\Singleton;
+
+class Sidebars
+{
+    use Singleton;
+
+    protected function __construct()
+    {
+        // load class.
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks()
+    {
+        /**
+        * Actions.
+        */
+        add_action('widgets_init', [$this, 'register_sidebars']);
+    }
+
+    public function register_sidebars()
+    {
+        register_sidebar([
+            'name' => __('Sidebar', 'spawordpress'),
+            'id' => 'sidebar-1',
+            'description' => __('Main Sidebar', 'spawordpress'),
+            'before_widget' => '<div id="%1$s" class="widget widget-sidebar %2$s">',
+            'before_widget' => '</div>',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>',
+        ]);
+
+        register_sidebar([
+            'name' => __('Footer', 'spawordpress'),
+            'id' => 'sidebar-2',
+            'description' => __('Footer Sidebar', 'spawordpress'),
+            'before_widget' => '<div id="%1$s" class="widget widget-footer %2$s">',
+            'before_widget' => '</div>',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>',
+        ]);
+    }
+}
