ec84dc77312ff77cd214dd88b5822bc0a09d1c63
DamoFD <voshall@yahoo.com>
2024-07-05 20:08:09 -0700

    meta box nonce

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -46,6 +46,11 @@ class Meta_Boxes
     {
         $value = get_post_meta($post->ID, '_hide_page_title', true);
 
+        /**
+        * Use nonce for verification
+        */
+        wp_nonce_field(plugin_basename(__FILE__), 'hide_title_meta_box_nonce_name');
+
         ?>
         <label for="spawordpress-field"><?php esc_html_e('Hide the page title', 'spawordpress'); ?></label>
         <select id="spawordpress-field" class="postbox" name="spawordpress_hide_title_field">
@@ -58,6 +63,21 @@ class Meta_Boxes
 
     public function save_post_meta_data($post_id)
     {
+        /**
+        * When the post is saved or updated we get $_POST available
+        * Check if the current user is authorized
+        */
+        if (!current_user_can('edit_post', $post_id)) {
+            return;
+        }
+
+        /**
+        * Check if the nonce value we received is the same we created.
+        */
+        if (!isset($_POST['hide_title_meta_box_nonce_name']) || !wp_verify_nonce($_POST['hide_title_meta_box_nonce_name'], plugin_basename(__FILE__))) {
+            return;
+        }
+
         if (array_key_exists('spawordpress_hide_title_field', $_POST)) {
             update_post_meta(
                 $post_id,
